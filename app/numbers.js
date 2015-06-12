75ff7b4a136bd42cbfb9f7fa6024d62f057349d6
Mike Goldsmith <goldy13_1992@hotmail.co.uk>
2015-06-12 17:37:31 +0100

    updated

--- app/numbers.js
+++ app/numbers.js
@@ -3,53 +3,76 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
 
-    	num = num >>> (bit-1);
-  	return num & 1;
+      num = num >>> (bit-1);
+    return num & 1;
   },
 
   base10: function(str) {
 
 
-  	var n = 1;
-  	var sum = 0; parseInt(str[str.length-1], 10);
-  	for (i = str.length - 2; i >= 0; i--)
-  	{
-  		var num = parseInt(str[i], 10);
+    var n = 1;
+    var sum = 0; parseInt(str[str.length-1], 10);
+    for (i = str.length - 2; i >= 0; i--)
+    {
+      var num = parseInt(str[i], 10);
 
-  		sum += Math.pow(num * 2, n);
-  		n++;
-  	}
+      sum += Math.pow(num * 2, n);
+      n++;
+    }
 
-  	return sum;
+    return sum;
   },
 
   convertToBinary: function(num) {
 
 //console.log(0.5 > 0);
-	var str = "";
+  var str = "";
 
-	while (num > 0)
-	{
-		num = num / 2;
+  while (num > 0)
+  {
+    num = num / 2;
 //console.log(num);
-		if (num % 1 == 0)
-			str = "0" + str;
+    if (num % 1 == 0)
+      str = "0" + str;
 
-		else
-		{
-			str = "1" + str;
-			num -= 0.5;
-		}
+    else
+    {
+      str = "1" + str;
+      num -= 0.5;
+    }
 
-	}
+  }
 
-	while (str.length < 8)
-		str = "0" + str;		
-			return str;
+  while (str.length < 8)
+    str = "0" + str;    
+      return str;
 
   },
 
   multiply: function(a, b) {
+  var a1 = precision(a);
+  var b1 = precision(b);
 
+console.log ("a: " + a1 + ", b: " + b1 );
+  var pres = 0;
+
+  if (a1 > b1)
+  pres = a1;
+
+  else pres = b1;
+
+  var ans = a * b;
+  return parseFloat(ans.toFixed(pres));  
   }
 };
+
+function precision(n)
+{
+  n = n.toString();
+  for (i = 0; i < n.length; i++)
+    if (n[i] === ".")
+      return n.length -1 - i;
+
+  return -1;
+}
+
--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,23 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+	return fn.call(obj, obj.name, obj.greeting);
   },
 
   alterObjects : function(constructor, greeting) {
-
+  	constructor.prototype.greeting = greeting;
+  	return constructor;
   },
 
   iterate : function(obj) {
+	var x = Object.getOwnPropertyNames(obj);
 
+	var arr = [];
+
+	for (var i = 0; i < x.length; i++) {
+		var s = x[i] + ": " + obj[x[i]];
+		arr.push(s);
+	};
+  	return  arr;
   }
 };
